cd18522046a5756491170b0a7c2e144552ffcdac
shakeelphaedra <shakeel@phaedrasolutions.com>
2020-04-23 15:59:14 +0500

    dev environment database changes

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 module Peterpitch
   class Application < Rails::Application
 
-    config.active_job.queue_adapter = :delayed_job
+    # config.active_job.queue_adapter = :delayed_job
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.autoload_paths += %W(#{config.root}/lib)
