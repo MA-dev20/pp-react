d0c83e82cecd9437cf07061dd54146b66bd7c9b7
Resing1990 <33636566+Resing1990@users.noreply.github.com>
2020-08-06 20:02:36 +0200

    add content + create/edit lists

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,5 +1,6 @@
 class ListController < ApplicationController
   before_action :set_user
+  before_action :set_list, only: [:update, :add_entry]
 
   def create
     if params[:list][:type] == 'catchword'
@@ -16,9 +17,63 @@ class ListController < ApplicationController
       return
     end
   end
+  def update
+    @list.update(list_params)
+    render json: {id: @list.id, type: @list_type}
+  end
+  def destroy
+    if @list.destroy
+      render json: {success: true}
+    else
+      render json: {error: true}
+    end
+  end
+
+  def add_entry
+    if @list_type == 'catchword'
+      @entry = @user.catchwords.find_by(list_params)
+      @entry = @company.catchwords.create(user: @user, name: params[:list][:name]) if !@entry
+      @list.catchwords << @entry
+    else
+      @entry = @user.objections.find_by(list_params)
+      @entry = @company.objections.create(user: @user, name: params[:list][:name]) if !@entry
+      @list.objections << @entry
+    end
+    render json: {list: @list.id, entry: @entry.name, entry_id: @entry.id}
+  end
+
+  def edit_entry
+    if params[:type] == 'catchword'
+      @entry = Catchword.find_by(id: params[:entry])
+      @entry.update(sound: params[:sound]);
+    else
+      @entry = Objection.find_by(id: params[:entry])
+      @entry.update(sound: params[:sound]);
+    end
+    render json: {entry: @entry.id, type: params[:type]}
+  end
+
+  def delete_entry
+    if params[:type] == 'catchword'
+      @entry = Catchword.find_by(id: params[:entry_id])
+    else
+      @entry = Objection.find_by(id: params[:entry_id])
+    end
+    @entry.destroy
+    render json: {success: true}
+  end
   private
     def list_params
-      params.require(:list).permit(:name)
+      params.require(:list).permit(:name, :content_folder_id)
+    end
+    def set_list
+      if params[:list][:type] == 'catchword'
+        @list = CatchwordList.find(params[:list_id])
+        @list_type = 'catchword'
+      else
+        @list = ObjectionList.find(params[:list_id])
+        @list_type = 'objection'
+      end
     end
     def set_user
       if user_signed_in? && company_logged_in?
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,17 @@ class User < ApplicationRecord
   has_many :catchwords, dependent: :destroy
   has_many :task_media, dependent: :destroy
   has_many :content_folders, dependent: :destroy
+  has_many :catchword_lists, dependent: :destroy
+  has_many :catchwords, dependent: :destroy
+  has_many :objection_lists, dependent: :destroy
+  has_many :objections, dependent: :destroy
 
   has_many :teams, dependent: :destroy
   has_many :team_users, dependent: :destroy
   has_many :games, dependent: :destroy
   has_many :game_users, dependent: :destroy
-  has_many :catchword_lists, dependent: :destroy
-  has_many :objection_lists, dependent: :destroy
+
+
   has_many :rating_lists, dependent: :destroy
   has_one :do_and_dont, dependent: :destroy
   has_many :game_turns, dependent: :destroy
