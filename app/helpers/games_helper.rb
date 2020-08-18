ba12ff88d8a363b75194be67083f01de107f75dc
Jan <jan.resing@gmail.com>
2020-08-18 14:37:54 +0200

    objection design

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -20,41 +20,41 @@ module GamesHelper
 	  @ol.objections << @temp.objections
 	end
   end
-	
+
   def generate_qr(text)
     qrcode = RQRCode::QRCode.new( text, :size => 5 )
 	html = qrcode.as_html
   end
-	
+
   def game_login(game)
 	session[:game_id] = game.id
   end
-	
+
   def current_game
 	@current_game ||= Game.find_by(id: session[:game_id])
   end
-	
+
   def game_logged_in?
 	!current_game.nil?
   end
-	
+
   def game_logout
 	session.delete(:game_id)
 	@current_game = nil
   end
-	
+
   def game_user_login(user)
 	session[:game_user_id] = user.id
   end
-	
+
   def current_game_user
 	@current_game_user ||= User.find_by(id: session[:game_user_id])
   end
-	
+
   def game_user_logged_in?
 	!current_game_user.nil?
   end
-	
+
   def game_user_logout
 	session.delete(:game__user_id)
 	@current_game = nil
